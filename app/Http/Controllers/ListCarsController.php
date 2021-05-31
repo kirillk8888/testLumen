5c5720336728990ef9e2153681f725b2b854560b
Кирилл Козлов <kirillk8888@googlemail.com>
2021-05-31 14:40:52 +0500

    additions

--- app/Http/Controllers/ListCarsController.php
+++ app/Http/Controllers/ListCarsController.php
@@ -26,6 +26,11 @@ class ListCarsController extends Controller
     public function update ($id, Request $request) {
         //проверяем даты использования по таблице
         $dateStart = $request->get('dateStart');
+
+        //если не задали дату начала, то ставим текущую
+        if (!$dateStart || $dateStart == '') {
+            $dateStart = date("Y-m-d H:i:s");
+        }
         $dateEnd = $request->get('dateEnd');
         $countUsedCar =  UsedCars::where('car_id', $id) -> where(function ($query) use ($dateStart, $dateEnd) {
             $query->where(function ($q) use ($dateStart, $dateEnd) {
@@ -46,33 +51,32 @@ class ListCarsController extends Controller
             });
 
         })->count();
+        if ($countUsedCar != 0) {
+            return response()-> json(['Error'=> 'Error date'], 200);
+        }
         //если не найдено полей в диапазоне дат:
-        if ($countUsedCar == 0) {
-            $car = Cars::findOrFail($id);
-            if ($request->toArray()['status'] == 'confirm' || $request->get('status') == 'check' && empty($car['user_id']) && $car['user_id'] == NULL) {
-                //проверяем, привязан ли уже к авто юзер
-                $user = Cars::where('user_id', $request->get('user_id'))->first();
-                //если привязан, то вначале убираем привязку
-                if ($user) {
-                    $user->fill(['user_id' => NULL])->save();
-                }
-                //а затем привязываем авто к юзеру
-                $usedCars = new UsedCars;
-                $usedCars->fill(array(
-                    'dataStart'  => $dateStart,
-                    'dataEnd' => $dateEnd,
-                    'user_id'   => $request->get('user_id'),
-                    'car_id'   => $id
-                ))->save();
-                $car -> fill(['user_id' => $request->get('user_id')])->save();
-                return response()-> json($car, 200);
+        $car = Cars::findOrFail($id);
+        if ($request->toArray()['status'] == 'confirm' || $request->get('status') == 'check' && empty($car['user_id']) && $car['user_id'] == NULL) {
+            //проверяем, привязан ли уже к авто юзер
+            $user = Cars::where('user_id', $request->get('user_id'))->first();
+            //если привязан, то вначале убираем привязку
+            if ($user) {
+                $user->fill(['user_id' => NULL])->save();
             }
-            else {
-                return response('auto tied to user', 200); //если авто уже привязано к пользователю
-            }
+            //а затем привязываем авто к юзеру
+            $usedCars = new UsedCars;
+            $usedCars->fill(array(
+                'dataStart'  => $dateStart,
+                'dataEnd' => $dateEnd,
+                'user_id'   => $request->get('user_id'),
+                'car_id'   => $id
+            ))->save();
+            $car -> fill(['user_id' => $request->get('user_id')])->save();
+            return response()-> json($car, 200);
         }
         else {
-            return response()-> json(['Error'=> 'Error date'], 200);
-        };
+            return response(['Error'=> 'Auto tied to user'], 404); //если авто уже привязано к пользователю
+        }
+
     }
 }
--- app/Http/Controllers/ListUserController.php
+++ app/Http/Controllers/ListUserController.php
@@ -27,22 +27,20 @@ class ListUserController extends Controller
         return response() ->json($users) ;
     }
     public function update ($id, Request $request) {
-        $cars = Cars::where('user_id', $id)->first();
-
-        if ($cars) {
-            $cars->fill(['user_id' => NULL]);
-            $cars->save();
-        }
-
         $car_id = $request->get('car_id');
         $dateStart = $request->get('dateStart');
+
+        //если не задали дату начала, то ставим текущую
+        if (!$dateStart || $dateStart == '') {
+            $dateStart = date("Y-m-d H:i:s");
+        }
         $dateEnd = $request->get('dateEnd');
         $car_id = $request->get('car_id');
         $cars = Cars::find($car_id);
         if ($cars) {
             $usedCars = new UsedCars;
             //проверяем даты использования по таблице
-            $countUsedCar =  UsedCars::where('car_id', $id) -> where(function ($query) use ($dateStart, $dateEnd) {
+            $countUsedCar =  UsedCars::where('car_id', $car_id) -> where(function ($query) use ($dateStart, $dateEnd) {
                 $query->where(function ($q) use ($dateStart, $dateEnd) {
                     $q->where('dataStart', '>=', $dateStart)
                         ->where('dataStart', '<', $dateEnd);
@@ -61,19 +59,25 @@ class ListUserController extends Controller
                 });
 
             })->count();
-            //если не найдено полей в диапазоне дат:
-            if ($countUsedCar == 0) {
-                //заполняем таблицу использования авто
-                $usedCars->fill(array(
-                    'dataStart'  => $dateStart,
-                    'dataEnd' => $dateEnd,
-                    'user_id'   => $id,
-                    'car_id'   => $car_id
-                ))->save();
-                //обновляем поле user_id в таблице авто
-                $cars->fill(['user_id' => $id])->save();
-                return response()-> json($cars, 200);
+            if ($countUsedCar != 0) {
+                return response()-> json(['Error'=> 'Error date'], 200);
             }
+            //если не найдено полей в диапазоне дат:
+            $carsUser = Cars::where('user_id', $id)->first();
+            if ($carsUser) {
+                $carsUser->fill(['user_id' => NULL]);
+                $carsUser->save();
+            }
+            //заполняем таблицу использования авто
+            $usedCars->fill(array(
+                'dataStart'  => $dateStart,
+                'dataEnd' => $dateEnd,
+                'user_id'   => $id,
+                'car_id'   => $car_id
+            ))->save();
+            //обновляем поле user_id в таблице авто
+            $cars->fill(['user_id' => $id])->save();
+            return response()-> json($cars, 200);
         }
         else {
             return response()-> json(['Error'=> 'Id not found'], 404);
